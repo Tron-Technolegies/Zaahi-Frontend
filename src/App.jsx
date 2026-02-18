98d49442822cbc9e47005dfae6f67a540e117eaf
ayana <ayana.anandhan@gmail.com>
2026-02-18 11:45:32 +0530

    added first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Children } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home, SignIn, SignUp } from './Pages';
+import HomeLayout from './Pages/HomeLayout';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
