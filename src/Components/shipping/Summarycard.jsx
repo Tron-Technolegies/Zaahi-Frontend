615a7351725249c2a396d18e0e4e80963dc55346
Afrajasira123 <afrajasira20@gmail.com>
2026-02-28 22:27:00 +0530

    shipping & review done w.responsive

---
+++ src/Components/shipping/Summarycard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Picture from "../Productdetails/Picture";
+
+const Summarycard = () => {
+  return (
+    <div className="w-full flex justify-center lg:block">
+      <div className="border border-gray-400 py-7 px-5 bg-gray-200 w-full max-w-sm lg:w-80">
+        <p className="font-[Bastoni] text-sm">Order Summary</p>
+
+        <div className="flex gap-5 border-b border-gray-500 pb-6 mt-6">
+          <div className="w-16 h-20 border border-gray-300 bg-gray-100 flex items-center justify-center">
+            <img src="/Featured/Lehanga.png" alt="product" className="object-contain h-full" />
+          </div>
+
+          <div className="flex flex-col text-sm font-[Inter]">
+            <p className="tracking-widest font-[Be Vietnam Pro]">Raw Silk Dupatta</p>
+            <p className="text-gray-400">Qty:1</p>
+            <p>$12,500</p>
+          </div>
+        </div>
+
+        <div className="space-y-4 text-gray-600 text-sm mt-6 font-[Inter]">
+          <div className="flex justify-between">
+            <span>Subtotal</span>
+            <span>$12,500</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span>Shipping</span>
+            <span>Free</span>
+          </div>
+
+          <div className="flex justify-between border-b border-gray-500 pb-4">
+            <span>Tax (15%)</span>
+            <span>$625</span>
+          </div>
+        </div>
+
+        <div className="flex justify-between text-sm font-semibold mt-4">
+          <span>Total</span>
+          <span>$13,125</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Summarycard;
