e7af65fe6626cebc84dd85ca04b461307b339788
Afrajasira123 <afrajasira20@gmail.com>
2026-03-04 23:33:19 +0530

    profile done

--- src/Pages/MyProfile.jsx
+++ src/Pages/MyProfile.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import HeadName from "../Components/myprofile/HeadName";
+import ProfileDetails from "../Components/myprofile/ProfileDetails";
 
 const MyProfile = () => {
-  return <div>MyProfile</div>;
+  return (
+    <div>
+      <Header />
+      <HeadName />
+      <ProfileDetails />
+      <Footer />
+    </div>
+  );
 };
 
 export default MyProfile;
