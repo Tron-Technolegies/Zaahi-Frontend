1ba27480e266d3115e04290d008076530a03eb83
ayana <ayana.anandhan@gmail.com>
2026-03-08 12:57:07 +0530

    login register avatar logout

---
+++ src/hooks/auth/useSignin.js
@@ -0,0 +1,72 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
+import { toast } from 'react-toastify';
+
+export const useSignin = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async (data) => {
+      await api.post(`/auth/login`, data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['currentUser']);
+      navigate('/');
+      toast.success('Success');
+    },
+    onError: (error) => {
+      toast.error(
+        error?.response?.data?.error ||
+          error?.response?.data?.message ||
+          error.message,
+      );
+    },
+  });
+  return { isPending, mutateAsync };
+};
+
+export const useSignUp = () => {
+  const navigate = useNavigate();
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async (data) => {
+      await api.post(`/auth/register`, data);
+    },
+    onSuccess: () => {
+      toast.success('Success');
+      navigate('/signin');
+    },
+    onError: (error) => {
+      toast.error(
+        error?.response?.data?.error ||
+          error?.response?.data?.message ||
+          error.message,
+      );
+    },
+  });
+  return { isPending, mutateAsync };
+};
+
+export const useSignOut = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async () => {
+      await api.post('/auth/logout');
+    },
+    onSuccess: () => {
+      queryClient.removeQueries(['currentUser']);
+      navigate('/');
+      toast.success('Logged Out Successfully');
+    },
+    onError: (error) => {
+      toast.error(
+        error?.response?.data?.error ||
+          error?.response?.data?.message ||
+          error.message,
+      );
+    },
+  });
+  return { isPending, mutateAsync };
+};
