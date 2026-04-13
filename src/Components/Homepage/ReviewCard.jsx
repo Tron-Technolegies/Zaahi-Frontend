030f8a9894be015377920a5030ea370c97061170
clawlynx <clawlynx77@gmail.com>
2026-04-13 14:54:16 +0530

    api route change

--- src/Components/Homepage/ReviewCard.jsx
+++ src/Components/Homepage/ReviewCard.jsx
@@ -14,7 +14,7 @@ export default function ReviewCard({ img, review, rating, user }) {
           className="w-full h-full object-cover rounded-xl transform group-hover:scale-105 transition-transform duration-500"
         />
         {/* Gradient Overlay */}
-        <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+        <div className="absolute inset-0 bg-linear-to-t from-black/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
       </div>
 
       {/* Title and Button */}
